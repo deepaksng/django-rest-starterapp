4e7427f06e13112f8fb16a4a49b3a63b4a93714b
Deepak <jgecdeepak@gmail.com>
2024-08-15 02:44:07 +0530

    initial commit

---
+++ config/django/local.py
@@ -0,0 +1,21 @@
+from .base import *
+# from .base import INSTALLED_APPS
+
+DEBUG = True
+
+# Testing
+# INSTALLED_APPS += ('django_nose',)
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# NOSE_ARGS = [
+#     BASE_DIR,
+#     '-s',
+#     '--nologcapture',
+#     '--with-coverage',
+#     '--with-progressive',
+#     '--cover-package=piedpiper'
+# ]
+
+# Mail
+# EMAIL_HOST = 'localhost'
+# EMAIL_PORT = 1025
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
